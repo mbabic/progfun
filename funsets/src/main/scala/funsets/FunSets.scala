4ad588d3dc6486b879abdcc77ebc05508fe54aca
mbabic <mbabic@ualberta.ca>
2014-05-06 15:05:03 -0600

    Assignment complete, though not yet thoroughly tested.

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -20,30 +20,35 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-  def singletonSet(elem: Int): Set = ???
+  def singletonSet(elem: Int): Set = 
+    (x => if (x == elem) true else false)
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-  def union(s: Set, t: Set): Set = ???
+  def union(s: Set, t: Set): Set = 
+    (x => s(x) || t(x))
 
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-  def intersect(s: Set, t: Set): Set = ???
-
+  def intersect(s: Set, t: Set): Set = 
+    (x => s(x) && t(x))
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-  def diff(s: Set, t: Set): Set = ???
+  def diff(s: Set, t: Set): Set = 
+    (x => s(x) && !t(x))
+
 
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-  def filter(s: Set, p: Int => Boolean): Set = ???
+  def filter(s: Set, p: Int => Boolean): Set = 
+    (x => s(x) && p(x))
 
   /**
    * The bounds for `forall` and `exists` are +/- 1000.
@@ -52,26 +57,35 @@ object FunSets {
 
   /**
    * Returns whether all bounded integers within `s` satisfy `p`.
+   * Strategy: iterate through all integers in interval [-bound, bound].  If
+   * any element of s fails to satisfy the predicate, return false.  Else,
+   * if we iterate through all elements in interval without having found
+   * false instance, return true
    */
   def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a > bound) true
+      else if (s(a) && !p(a)) false
+      else iter(a + 1)
     }
-    iter(???)
+    iter(-bound)
   }
 
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-  def exists(s: Set, p: Int => Boolean): Boolean = ???
+  def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, (x => !p(x)))
 
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
+   * Idea behind implementation:
+   * let s' be set to be returned.
+   * x in s' <=> there exists y in s such that y = f(x).  The logical expression
+   * is the idea behind the implementaiton given below.
    */
-  def map(s: Set, f: Int => Int): Set = ???
+  def map(s: Set, f: Int => Int): Set = 
+    (x => exists(s, (y => y == f(x))))
 
   /**
    * Displays the contents of a set
