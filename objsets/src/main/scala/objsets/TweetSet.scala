ff3067e42a809a9feca8bddb871895d0616f595b
mbabic <mbabic@ualberta.ca>
2014-05-12 13:30:48 -0600

    Empty.union() and NonEmpty.union() implemented and passing unit tests.

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -38,12 +38,11 @@ abstract class TweetSet {
   /**
    * This method takes a predicate and returns a subset of all the elements
    * in the original set for which the predicate is true.
-   *
-   * Question: Can we implment this method here, or should it remain abstract
-   * and be implemented in the subclasses?
    */
   def filter(p: Tweet => Boolean): TweetSet = 
     filterAcc(p, new Empty)
+    
+  def isEmpty: Boolean
 
   /**
    * This is a helper method for `filter` that propagetes the accumulated tweets.
@@ -52,12 +51,9 @@ abstract class TweetSet {
 
   /**
    * Returns a new `TweetSet` that is the union of `TweetSet`s `this` and `that`.
-   *
-   * Question: Should we implment this method here, or should it remain abstract
-   * and be implemented in the subclasses?
    */
-   def union(that: TweetSet): TweetSet = ???
-
+   def union(that: TweetSet): TweetSet
+   
   /**
    * Returns the tweet from this set which has the greatest retweet count.
    *
@@ -110,9 +106,14 @@ abstract class TweetSet {
 }
 
 class Empty extends TweetSet {
+  
+  def isEmpty = true
 
   def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc
 
+  def union(that: TweetSet): TweetSet =
+    that
+  
   /**
    * The following methods are already implemented
    */
@@ -128,9 +129,15 @@ class Empty extends TweetSet {
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
+  def isEmpty = false
+  
   def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = 
     if (p(elem)) left filterAcc (p, right filterAcc(p, acc incl elem))
     else left filterAcc(p, right filterAcc(p, acc))
+  
+  
+  def union(that: TweetSet): TweetSet = 
+  	filterAcc(x => true, that)
 
   def contains(x: Tweet): Boolean =
     if (x.text < elem.text) left.contains(x)
